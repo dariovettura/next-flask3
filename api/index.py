199ace135385aa1433a4d7ad6587eb5276d17312
dariovettura <d.vettura.wd@gmail.com>
2024-09-25 12:54:24 +0200

    ok

--- api/index.py
+++ api/index.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from flask import Blueprint, request, jsonify
 app = Flask(__name__)
 
-@app.route("/api/python")
+@app.route("/api/python", methods=["GET","POST"])
 def hello_world():
-    return "<p>Hello, World!</p>"
+    data = request.get_json()
+    
+    # Assicurati che ci siano dati
+    if data:
+        # Aggiungi "ricevuto" all'input
+        response = f"{data.get('input', '')} ricevuto"
+        return jsonify({"response": response}), 200
+    else:
+        return jsonify({"error": "Nessun dato ricevuto"}), 400
