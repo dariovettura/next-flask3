f72a25cead1c36cfc5511fef326b0670b07f61cb
dariovettura <d.vettura.wd@gmail.com>
2024-09-25 14:44:06 +0200

    vai

--- api/index.py
+++ api/index.py
@@ -1,13 +1,15 @@
-from flask import Flask
-from flask_cors import CORS
 
-from test import test_bp     
+from flask import Flask
+
+from test import test_bp   
 
 app = Flask(__name__)
-CORS(app)
-# Registrazione dei blueprints
 
 app.register_blueprint(test_bp)
+
+@app.route("/api/python", methods=["GET", "POST"])
+def hello_world():
+   return "Hello, World!"
 
 if __name__ == "__main__":
     app.run(debug=True)
