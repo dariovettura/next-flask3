e189ccd5c5812a6a965d0412f8f4ae5a09d8f2ae
dariovettura <d.vettura.wd@gmail.com>
2024-09-25 13:48:57 +0200

    bo

--- api/index.py
+++ api/index.py
@@ -4,8 +4,10 @@ from .test import test_blueprint
 app = Flask(__name__)
 
 @app.route("/api/python", methods=["GET","POST"])
-def hello_world():
-    return
+def hello_test():
+    return "<p>Ciao, World!</p>"
+
+
 app.register_blueprint(test_blueprint)
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
