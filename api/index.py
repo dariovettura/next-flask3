892f7c47b22a25c9c32a8ac00395c2acb2145a45
dariovettura <d.vettura.wd@gmail.com>
2024-09-25 13:55:05 +0200

    non so

--- api/index.py
+++ api/index.py
@@ -1,13 +1,15 @@
-from flask import Flask
-from flask import Blueprint, request, jsonify
+# api/index.py
+from flask import Flask, request, jsonify
 from .test import test_blueprint 
+
 app = Flask(__name__)
 
-@app.route("/api/python")
-def hello_test():
+@app.route("/api/python", methods=["GET", "POST"])
+def hello_world():
     return "<p>Ciao, World!</p>"
 
-
+# Registra il blueprint
 app.register_blueprint(test_blueprint)
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
